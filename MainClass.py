57dd91bc5526f3f1dd73380b4da9ac6c8b36b6bb
Your Name <i.kabbash@protonmail.ch>
2021-05-18 20:10:57 +0200

    Uploaded pass1

---
+++ MainClass.py
@@ -0,0 +1,14 @@
+# Welcome to the SICXE project for the Systems Programming course
+# Supervised by: Dr. Sherine Nagi & Eng. Esraa Khattab
+# Group canditates:
+# Mazen Mamdouh - 17100759
+# Ibrahim M. Kabbash - 17100399
+
+import re
+import ToolsFile
+import pass1
+
+mainArray = ToolsFile.appendall()
+print(mainArray)
+pass1.locationCounter()
+pass1.SymbolTable()
